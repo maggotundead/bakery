c8cc62d7cf11ae5ba874d8009130967b84fcbe2f
Roman Salko <maggotundead@yandex.ua>
2022-04-12 19:10:27 +0300

    bakery beta

---
+++ resources/js/main.js
@@ -0,0 +1,59 @@
+
+const productsSwiper = new Swiper('.js-products-swiper', {
+    slidesPerView: 'auto',
+    loop: false,
+    draggable: true,
+    mousewheel: {
+        forceToAxis: true,
+    },
+    noSwiping: true,
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true
+    },
+});
+
+const reviewsSlider = new Swiper('.js-reviews-swiper', {
+    slidesPerView: 'auto',
+    loop: false,
+    draggable: true,
+    mousewheel: {
+        forceToAxis: true,
+    },
+    noSwiping: true,
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true
+    },
+});
+
+const gallerySlider = new Swiper('.js-gallery-swiper', {
+    slidesPerView: 'auto',
+    loop: false,
+    draggable: true,
+    mousewheel: {
+        forceToAxis: true,
+    },
+    noSwiping: true,
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true
+    },
+});
+
+
+
+document.querySelector('.burger').addEventListener('click', () => {
+    document.querySelector('body').classList.toggle('mobile-menu-open');
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.body.classList.contains('mobile-menu-open') && document.body.classList.remove('mobile-menu-open');
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
